a2bdba6686b6d77f954287396b749a36fb4875ba
Timo Schöpflin <timo@schoepflin.io>
2021-04-30 12:55:11 +0200

    Cut question at first occurrence of a point or after 10 characters

--- src/bag_faq_crawler.py
+++ src/bag_faq_crawler.py
@@ -6,6 +6,7 @@ from watson import WatsonWrapper
 
 class BagFaqCrawler:
     BAG_MARKER = ' (BAG)'
+    MAX_ANSWER_LENGTH = 10
     def __init__(self, watson_api_key, watson_skill_id, watson_workspace_url, bag_faq_url):
         self.watson = WatsonWrapper(watson_skill_id, watson_api_key, watson_workspace_url)
         self.crawler = Crawler(bag_faq_url, self.callback)
@@ -25,17 +26,26 @@ class BagFaqCrawler:
 
     def callback(self, uuid, link, question, answer):
         question_with_marker = question + self.BAG_MARKER
+        short_answer = self.shorten_answer(answer)
         if uuid not in self.existing_dialog_nodes.keys():
             logging.info("Adding " + uuid + "to watson assitant")
             self.watson.createIntent(uuid, question_with_marker)
-            self.watson.createDialogNode(uuid, question_with_marker, answer)
-        elif self.existing_entry_has_changed(self.existing_dialog_nodes[uuid], question_with_marker, answer):
+            self.watson.createDialogNode(uuid, question_with_marker, short_answer)
+        elif self.existing_entry_has_changed(self.existing_dialog_nodes[uuid], question_with_marker, short_answer):
             logging.info("Question or Answer for " + uuid + " changed, therefore updating watson assistant.")
-            self.update_watson(uuid, question_with_marker, answer)
+            self.update_watson(uuid, question_with_marker, short_answer)
             del self.existing_dialog_nodes[uuid]
         else:
             logging.info("Not adding " + uuid + " to watson assistant, since it's already present.")
             del self.existing_dialog_nodes[uuid]
+
+    def shorten_answer(self, answer):
+        cut_index = answer.find('.')
+        if cut_index <= 0 and self.MAX_ANSWER_LENGTH < len(answer):
+            cut_index = self.MAX_ANSWER_LENGTH
+        elif cut_index <= 0:
+            cut_index = len(answer)
+        return answer[0:cut_index+1] + ' ...'
 
     def existing_entry_has_changed(self, existing_dialog_node, question, answer):
         return question != existing_dialog_node['question'] or answer != existing_dialog_node['answer']
--- tests/bag_faq_crawler_test.py
+++ tests/bag_faq_crawler_test.py
@@ -17,16 +17,30 @@ class TestBagFaqCrawler(unittest.TestCase):
         self.bag_faq_crawler.crawl()
         self.watson_wrapper.create_dialog_folder.assert_called()
 
-    def test_create_new(self):
+    def test_create_new_cut_at_point(self):
+        self.watson_wrapper.listDialogNodes = unittest.mock.Mock(return_value={'faq': {}})
+        self.crawler.crawl = lambda: self.bag_faq_crawler.callback('test', '', 'question', 'answer. more details')
+        self.bag_faq_crawler.crawl()
+        self.watson_wrapper.createIntent.assert_called_with('test', 'question (BAG)')
+        self.watson_wrapper.createDialogNode.assert_called_with('test', 'question (BAG)', 'answer. ...')
+
+    def test_create_new_cut_at_length(self):
+        self.watson_wrapper.listDialogNodes = unittest.mock.Mock(return_value={'faq': {}})
+        self.crawler.crawl = lambda: self.bag_faq_crawler.callback('test', '', 'question', 'answer with some more details')
+        self.bag_faq_crawler.crawl()
+        self.watson_wrapper.createIntent.assert_called_with('test', 'question (BAG)')
+        self.watson_wrapper.createDialogNode.assert_called_with('test', 'question (BAG)', 'answer with ...')
+
+    def test_create_new_not_cut(self):
         self.watson_wrapper.listDialogNodes = unittest.mock.Mock(return_value={'faq': {}})
         self.crawler.crawl = lambda: self.bag_faq_crawler.callback('test', '', 'question', 'answer')
         self.bag_faq_crawler.crawl()
         self.watson_wrapper.createIntent.assert_called_with('test', 'question (BAG)')
-        self.watson_wrapper.createDialogNode.assert_called_with('test', 'question (BAG)', 'answer')
+        self.watson_wrapper.createDialogNode.assert_called_with('test', 'question (BAG)', 'answer ...')
 
     def test_no_change_since_node_already_exists(self):
         self.watson_wrapper.listDialogNodes = unittest.mock.Mock(
-            return_value={'faq': {}, 'test': {'uuid': 'test', 'question': 'question (BAG)', 'answer': 'answer'}})
+            return_value={'faq': {}, 'test': {'uuid': 'test', 'question': 'question (BAG)', 'answer': 'answer ...'}})
         self.crawler.crawl = lambda: self.bag_faq_crawler.callback('test', '', 'question', 'answer')
         self.bag_faq_crawler.crawl()
         self.watson_wrapper.createIntent.assert_not_called()
@@ -34,29 +48,29 @@ class TestBagFaqCrawler(unittest.TestCase):
 
     def test_answer_changed_of_existing_node(self):
         self.watson_wrapper.listDialogNodes = unittest.mock.Mock(
-            return_value={'faq': {}, 'test': {'uuid': 'test', 'question': 'question (BAG)', 'answer': 'answer'}})
+            return_value={'faq': {}, 'test': {'uuid': 'test', 'question': 'question (BAG)', 'answer': 'answer ...'}})
         self.watson_wrapper.get_intent = unittest.mock.Mock(
             return_value={'intent': 'test', 'examples': [{'text': 'question (BAG)'}, {'text': 'question (NOT_BAG)'}]})
         self.crawler.crawl = lambda: self.bag_faq_crawler.callback('test', '', 'question', 'answer2')
         self.bag_faq_crawler.crawl()
         self.watson_wrapper.update_intent.assert_called_with(
             {'intent': 'test', 'examples': [{'text': 'question (BAG)'}, {'text': 'question (NOT_BAG)'}], 'description': 'question (BAG)'})
-        self.watson_wrapper.update_dialog_node.assert_called_with('test', 'question (BAG)', 'answer2')
+        self.watson_wrapper.update_dialog_node.assert_called_with('test', 'question (BAG)', 'answer2 ...')
 
     def test_question_changed_of_existing_node(self):
         self.watson_wrapper.listDialogNodes = unittest.mock.Mock(
-            return_value={'faq': {}, 'test': {'uuid': 'test', 'question': 'question (BAG)', 'answer': 'answer'}})
+            return_value={'faq': {}, 'test': {'uuid': 'test', 'question': 'question (BAG)', 'answer': 'answer ...'}})
         self.watson_wrapper.get_intent = unittest.mock.Mock(
             return_value={'intent': 'test', 'examples': [{'text': 'question (BAG)'}, {'text': 'question (NOT_BAG)'}]})
         self.crawler.crawl = lambda: self.bag_faq_crawler.callback('test', '', 'question2', 'answer')
         self.bag_faq_crawler.crawl()
         self.watson_wrapper.update_intent.assert_called_with(
             {'intent': 'test', 'examples': [{'text': 'question2 (BAG)'}, {'text': 'question (NOT_BAG)'}], 'description': 'question2 (BAG)'})
-        self.watson_wrapper.update_dialog_node.assert_called_with('test', 'question2 (BAG)', 'answer')
+        self.watson_wrapper.update_dialog_node.assert_called_with('test', 'question2 (BAG)', 'answer ...')
 
     def test_delete_missing_faq_entry(self):
         self.watson_wrapper.listDialogNodes = unittest.mock.Mock(
-            return_value={'faq': {}, 'test': {'uuid': 'test', 'question': 'question (BAG)', 'answer': 'answer'}})
+            return_value={'faq': {}, 'test': {'uuid': 'test', 'question': 'question (BAG)', 'answer': 'answer ...'}})
         self.bag_faq_crawler.crawl()
         self.watson_wrapper.delete_dialog_node.assert_called_with('test')
         self.watson_wrapper.delete_intent.assert_called_with('test')
